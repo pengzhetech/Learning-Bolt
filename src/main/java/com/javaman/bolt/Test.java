542f5fc3c4588b8c3f1b7e66b43abdae7f9dc744
Tyrion <zhe.peng@lazada.com>
2019-06-21 21:45:31 +0800

    init

---
+++ src/main/java/com/javaman/bolt/Test.java
@@ -0,0 +1,13 @@
+package com.javaman.bolt;
+
+/**
+ * @author pengzhe
+ * @date 2019-06-21 21:42
+ * @description
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("13");
+    }
+}
